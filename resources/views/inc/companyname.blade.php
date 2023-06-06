887f8e935f98761f670d3572c0081a870c97cd11
aloysiusmatz <aloysiusmatz@gmail.com>
2023-06-06 07:12:11 +0000

    fix 6 june 2023

--- resources/views/inc/companyname.blade.php
+++ resources/views/inc/companyname.blade.php
@@ -1,7 +1,7 @@
 @if(Session::has('companyname'))
     {{ strtoupper(Session::get('companyname')) }}
 @else
-    @php( $companyname = \App\Models\Settings::find(1) )
+    @php( $companyname = \App\Models\settings::find(1) )
     {{ Session::put('companyname', $companyname->value) }} 
     {{ strtoupper($companyname->value) }} 
 @endif
